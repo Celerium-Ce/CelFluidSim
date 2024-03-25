130987c94d90347c715dc3897d3eef0036532da2
6umar9 <umar23324@iiitd.ac.in>
2024-03-25 20:50:20 +0530

    build?

---
+++ build/CMakeFiles/celfluidsim.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/celfluidsim.dir/src/main.cpp.o"
+  "CMakeFiles/celfluidsim.dir/src/main.cpp.o.d"
+  "celfluidsim"
+  "celfluidsim.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/celfluidsim.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ src/main.cpp
@@ -0,0 +1,7 @@
+#include "config.h"
+
+using namespace::std;
+
+int main(){
+	cout << "Hello World!" << endl;
+}
