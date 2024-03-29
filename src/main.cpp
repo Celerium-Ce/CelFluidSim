46229632245d0c9c1da6907c8cddcfd6487d54ce
6umar9 <umar23324@iiitd.ac.in>
2024-03-29 17:14:47 +0530

    chapter 5

--- src/main.cpp
+++ src/main.cpp
@@ -2,29 +2,53 @@
 #define nl '\n'
 using std::cout;
 using std::cin;
+void framebuffer_size_callback(GLFWwindow* window, int width, int height){
+glViewport(0, 0, width, height);
+}
 
+void changerace(GLFWwindow* window){
+	if (glfwGetKey(window,GLFW_KEY_SPACE) == GLFW_PRESS){
+		glClearColor(0.75f,0.01f,1.0f,0.5f);
+	}
+}
+void changerace2(GLFWwindow* window){
+	if (glfwGetKey(window,GLFW_KEY_TAB) == GLFW_PRESS){
+		glClearColor(0.0f,0.0f,1.0f,0.5f);
+	}
+}
 int main(){
-
-	GLFWwindow* window;
-
+	
 	if (!glfwInit()){
 		cout << "GLFW couldn't start" << nl;	
 		return -1;
 	}
-	window = glfwCreateWindow(640,480,"CelFluidSim",NULL,NULL);
-	glfwMakeContextCurrent(window);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	GLFWwindow* window = glfwCreateWindow(800,600,"CelFluidSim",NULL,NULL);
 
+	
+	if (window==NULL){
+		cout << "Window couldnt be created" << nl;
+		glfwTerminate();
+		return -1;
+	}
+
+	glfwMakeContextCurrent(window);
 	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)){
 		glfwTerminate();
 		return -1;
 	}
 
-	glClearColor(0.75f,0.01f,1.0f,0.5f);
-
+	glViewport(0,0,800,600);
+	glClearColor(0.0f,0.0f,0.0f,1.0f);
 	while (!glfwWindowShouldClose(window)){
+		changerace(window);
+		changerace2(window);
+		glfwSetFramebufferSizeCallback(window,framebuffer_size_callback);
 		glClear(GL_COLOR_BUFFER_BIT);	
-		glfwPollEvents();
 		glfwSwapBuffers(window);
+		glfwPollEvents();
 	}
 	glfwTerminate();
 	return 0;
